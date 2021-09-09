b66d5f42c56da1f123e744a8fbdd6eeab8296738
Pham Phu Dien <phamphudien@gmail.com>
2021-09-09 14:04:34 +0700

    ad responsive

--- js/home.js
+++ js/home.js
@@ -12,7 +12,7 @@ var hamburgerBTN = document.querySelector(".navbar-toggler-icon");
 hamburgerBTN.addEventListener("click", function () {
   hamburgerBTN.classList.toggle("rotate");
 });
-//Type writer text banner
+//TYPE WRITER BANNER ANIMATION
 class Typewriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
@@ -79,3 +79,82 @@ function init() {
   // Init Typewriter
   new Typewriter(txtElement, words, wait);
 }
+//SCROLL TO TOP
+var scrollToTop = document.querySelector(".scroll-top-btn");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 700) {
+    scrollToTop.classList.add("show");
+  }
+  if (window.scrollY < 400) {
+    scrollToTop.classList.remove("show");
+  }
+});
+
+scrollToTop.addEventListener("click", goToTop);
+
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+//OUR UNIVERSE ANIMATION
+var solarSystemSub = document.querySelector(".solar-system-sub");
+var planetsSub = document.querySelector(".planets-sub");
+var constellSub = document.querySelector(".constell-sub");
+var galaxiesSub = document.querySelector(".galaxies-sub");
+var sunInfoContent = document.querySelector(".sun-info-content");
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  console.log(window.outerWidth);
+  if (this.window.outerWidth > 1024) {
+    if (window.scrollY > 350) {
+      solarSystemSub.classList.add("show");
+      planetsSub.classList.add("show");
+    }
+
+    if (window.scrollY > 700) {
+      constellSub.classList.add("show");
+    }
+    if (window.scrollY > 850) {
+      galaxiesSub.classList.add("show");
+    }
+    if (window.scrollY > 600) {
+      sunInfoContent.classList.add("show");
+    }
+  } else if (window.outerWidth <= 1024 && window.outerWidth > 768) {
+    if (window.scrollY > 100) {
+      solarSystemSub.classList.add("show");
+      planetsSub.classList.add("show");
+    }
+    if (window.scrollY > 450) {
+      constellSub.classList.add("show");
+    }
+    if (window.scrollY > 600) {
+      galaxiesSub.classList.add("show");
+    }
+    if (window.scrollY > 300) {
+      sunInfoContent.classList.add("show");
+    }
+  } else {
+    if (window.scrollY > 0) {
+      solarSystemSub.classList.add("show");
+      planetsSub.classList.add("show");
+    }
+    if (window.scrollY > 300) {
+      constellSub.classList.add("show");
+    }
+    if (window.scrollY > 500) {
+      galaxiesSub.classList.add("show");
+    }
+    if (window.scrollY > 200) {
+      sunInfoContent.classList.add("show");
+    }
+  }
+  if (this.window.scrollY < 100) {
+    solarSystemSub.classList.remove("show");
+    planetsSub.classList.remove("show");
+    constellSub.classList.remove("show");
+    galaxiesSub.classList.remove("show");
+    sunInfoContent.classList.remove("show");
+  }
+});
